4d18bbfb240d978c88455c6293b73a04680b88a9
kai <lennybleeding@gmail.com>
2025-11-16 20:37:08 -0500

    Change title text from '@biaquista' to 'hai'

--- js/title.js
+++ js/title.js
@@ -1,5 +1,5 @@
         // Texto que será exibido no <title>
-        const titleText = "@biaquista";
+        const titleText = "hai";
         let index = 0;
 
         // Função para animar o título
